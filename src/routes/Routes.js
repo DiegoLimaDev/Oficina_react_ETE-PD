18f3bc1c223f8f7efa1874c028735a92044e5e05
Diego Lima <xxdiegolsxx@gmail.com>
2025-05-05 12:04:56 -0300

    fix: :rocket: router
    changed to HashRouter

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,6 +1,6 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import Home from '../pages/Home';
 
-const router = createBrowserRouter([{ path: '/', Component: Home }]);
+const router = createHashRouter([{ path: '/', Component: Home }]);
 
 export default router;
