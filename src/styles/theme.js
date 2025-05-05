6758e290eea0047cf1cb62862efab3fdcecfc516
Diego Lima <xxdiegolsxx@gmail.com>
2025-05-05 11:56:17 -0300

    feat: :sparkles: style
    theme file

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+const theme = {
+  sizes: {
+    oneRem: '1rem',
+    small: '2rem',
+    medium: '4rem',
+    big: '8rem',
+  },
+  colors: {
+    primary: '#012E40',
+    secondary: '#024959',
+    third: '#026773',
+    fourth: '#3CA6A6',
+    auxiliar: '#F2E3D5',
+    black: '#00040D',
+  },
+};
+
+export default theme;
