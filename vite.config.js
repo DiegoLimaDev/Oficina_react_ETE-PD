3093186acd47f60cea266badc9ac56a441cff424
Diego Lima <xxdiegolsxx@gmail.com>
2025-05-05 12:10:39 -0300

    fix: :rocket: config
    outDir changed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   base: 'Oficina_react_ETE-PD',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
 });
