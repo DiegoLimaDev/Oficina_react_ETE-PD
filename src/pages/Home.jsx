199b7fe71b08baab78557d2167f40ea603d95106
Diego Lima <xxdiegolsxx@gmail.com>
2025-05-05 11:55:51 -0300

    feat: :sparkles: pages
    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,46 @@
-import { Box } from '@mui/material';
+import { Box, Grid } from '@mui/material';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Card from '../components/Card';
+import theme from '../styles/theme';
 
 const Home = () => {
-  return <Box>Home</Box>;
+  const url = 'https://jsonplaceholder.typicode.com/todos';
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    await axios.get(url).then((res) => setData(res.data));
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Box flex={1} minHeight={'100vh'}>
+        <Box
+          display={'flex'}
+          flexWrap={'wrap'}
+          px={theme.sizes.small}
+          justifyContent={'flex-start'}
+        >
+          {data.map((item) => {
+            return (
+              <Box key={item.id} m={theme.sizes.oneRem}>
+                <Card item={item} />
+              </Box>
+            );
+          })}
+        </Box>
+      </Box>
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
