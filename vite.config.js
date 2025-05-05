0af47d89afc328532cf787bc0e4ce069fe1261a4
Diego Lima <xxdiegolsxx@gmail.com>
2025-05-05 12:21:38 -0300

    fix: :rocket: config
    deploy gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   base: 'Oficina_react_ETE-PD',
-  build: {
-    outDir: 'docs',
-  },
   plugins: [react()],
 });
